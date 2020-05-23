962798071d3e68ede078ef76bf7b0b1d87ffca04
Mathis Raguin <mathis@cri.epita.fr>
2020-05-23 20:18:11 +0200

    add support for config.php

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'db';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'password';
+$config['db']['dbname'] = 'xenforo';
+
+$config['fullUnicode'] = true;
