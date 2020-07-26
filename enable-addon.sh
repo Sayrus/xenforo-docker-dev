91d90735dd28f20ec1a684ce8773dbbca3b41255
Mathis Raguin <mathis@cri.epita.fr>
2020-07-26 16:08:02 +0200

    fix: shellcheck enable-addon

--- enable-addon.sh
+++ enable-addon.sh
@@ -13,5 +13,5 @@ fi
 
 echo "Enabling $1 in $XENFORO_FOLDER"
 
-ln -Tsf "$1/src" "$XENFORO_FOLDER/src/addons/$(basename $1)"
-ln -Tsf "$1/js" "$XENFORO_FOLDER/js/$(basename $1)"
+ln -Tsf "$1/src" "$XENFORO_FOLDER/src/addons/$(basename "$1")"
+ln -Tsf "$1/js" "$XENFORO_FOLDER/js/$(basename "$1")"
